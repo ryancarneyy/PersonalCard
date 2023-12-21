b07ac412d2fce96e39972c2cee62cf5009ddfbaf
ryancarney <ryancarney771@gmail.com>
2023-12-21 11:15:44 -0800

    new commit - added buttons for socials

---
+++ index.js
@@ -0,0 +1,15 @@
+function moreOrLess() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("about-me-more");
+    var btnText = document.getElementById("am-more-button");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
